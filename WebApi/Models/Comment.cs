f54459196df0201223fcb90f7ff403ac75d2f34b
azhadolfo <azhadolfo1@gmail.com>
2026-02-09 12:57:48 +0800

    Iniital commit

---
+++ WebApi/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+    
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+    public int? StockId { get; set; }
+
+    public Stock? Stock { get; set; }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .UseSnakeCaseNamingConvention());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
