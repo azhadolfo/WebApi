e74d3e938c886be6e37e651d4bf422fbf429ba13
azhadolfo <azhadolfo1@gmail.com>
2026-02-09 13:09:05 +0800

    moved files

---
+++ src/WebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :  base(options)
+    {
+        
+    }
+
+
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
